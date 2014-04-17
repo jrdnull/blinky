b26db6b0aacba8a3e09a9d6c6191d1dfc586241c
Jordon Smith <jrd@mockra.net>
2014-04-17 20:36:50 +0100

    Formatting

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -2,23 +2,22 @@
 
 var controllers = angular.module('blinky.controllers', ['ui.router']);
 
-controllers.controller('NewPasteCtrl', function ($scope, $state, $http) {
-  $scope.submit = function () {
-    $http.post('/api/pastes', $scope.paste).success(function (data) {
+controllers.controller('NewPasteCtrl', function($scope, $state, $http) {
+  $scope.submit = function() {
+    $http.post('/api/pastes', $scope.paste).success(function(data) {
       $state.go('paste', data, {});
     });
   }
 });
 
-controllers.controller('ViewPasteCtrl', function ($scope, $stateParams, $http) {
-  $scope.paste = { text: 'Paste not found' };
-  $http.get('/api/pastes/' + $stateParams.id).success(function (data) {
+controllers.controller('ViewPasteCtrl', function($scope, $stateParams, $http) {
+  $http.get('/api/pastes/' + $stateParams.id).success(function(data) {
     $scope.paste = data;
   });
-})
+});
 
-controllers.controller('RecentPastesCtrl', function ($scope, $http) {
-  $http.get('/api/pastes').success(function (data) {
+controllers.controller('RecentPastesCtrl', function($scope, $http) {
+  $http.get('/api/pastes').success(function(data) {
     $scope.pastes = data;
   });
 });
